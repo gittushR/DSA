a3e6822c145abcf0939c6880672722bd226cf115
Tushar Rathi <rathitushar021@gmail.com>
2023-03-06 16:14:12 +0530

    Time: 4 ms (73.18%), Space: 9.6 MB (38.05%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -3,19 +3,19 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         int n=arr.size();
         int s=0,e=n-1;
-        int missing= compute(arr[n-1],n);
         while(s<=e){
             int mid=s+(e-s)/2;
-            missing=compute(arr[mid],mid+1);
+            int missing=calculateMissingNums(arr[mid],mid+1);
             
             if(missing>=k)e=mid-1;
             else s=mid+1;
         }
         if(e==-1)return k;
-        else return arr[e]+(k-compute(arr[e],e+1));
+        else return arr[e]+(k-calculateMissingNums(arr[e],e+1));
     }
 private:
-    int compute(int actual, int expected){
+//this functiuon returns total number of missing elements till that index
+    int calculateMissingNums(int actual, int expected){
         return actual-expected;
     }
 };
