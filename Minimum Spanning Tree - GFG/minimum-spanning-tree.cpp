1758a2176094318b7bd6a8460b27906ffef6d51f
Tushar Rathi <rathitushar021@gmail.com>
2023-05-31 16:39:47 +0545

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -6,36 +6,54 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	int findPar(int node, vector<int>&par){
+        if(par[node]==node)return node;
+        return par[node]=findPar(par[node],par);
+    }
+    void unionByRank (int u, int v, vector<int> &rank, vector<int>&par){
+        int ult_u=findPar(u,par);
+        int ult_v=findPar(v,par);
+        if(ult_u==ult_v)return;
+        if(rank[ult_u]<rank[ult_v]){
+            par[ult_u]=ult_v;
+        }else if(rank[ult_v]<rank[ult_u]){
+            par[ult_v]=ult_u;
+        }else if(rank[ult_v]==rank[ult_u]){
+            par[ult_u]=ult_v;
+            rank[ult_v]++;
+        }
+    }
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> >pq;
+        vector<int> par(V);
+        for(int i=0;i<V;i++)par[i]=i;
         
-        vector<int> vis(V,0);
-        int size=0;
-        
-        pq.push({0,0});
-        
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int wt=it.first;
-            int node=it.second;
-            
-            if(vis[node]==1)continue;
-            vis[node]=1;
-            size+=wt;
-            
-            for(auto a:adj[node]){
-                int adjNode=a[0];
-                int edW=a[1];
-                if(!vis[adjNode]){
-                    pq.push({edW,adjNode});
-                }
+        vector<int> rank(V,0);
+        //{wt,{u,v}}
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                edges.push_back({wt, {node, adjNode}});
             }
         }
-        return size;
+        sort(edges.begin(), edges.end());
+        int sum=0;
+        for (auto it : edges) {
+            int u=it.second.first;
+            int v=it.second.second;
+            int wt=it.first;
+            //cout<<"*******"<<findPar(u,par)<<"*******"<<findPar(v,par)<<"*******"<<wt<<u<<v<<endl;
+            if(findPar(u,par)!=findPar(v,par)){
+                sum+=wt;
+                //cout<<"****"<<sum<<"****"<<endl;
+                unionByRank(u,v,rank,par);
+            }
+        }
+        return sum;
     }
 };
 
