80514683c1f8f25de5176ec06192ef3ea3ed961a
Tushar Rathi <rathitushar021@gmail.com>
2023-06-07 13:56:11 +0545

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -3,10 +3,39 @@
 using namespace std;
 
 // } Driver Code Ends
-
 class Solution {
   public:
-    int helper(int day, int last, vector<vector<int>>& points, vector<vector<int>>& dp){
+    int f(int day, int last, vector<vector<int>>& points, vector<vector<int>>& dp){
+        if(dp[day][last] != -1) return dp[day][last];
+        if(day == 0){
+            int maxi = 0;
+            for(int i=0;i<3;i++){
+                if(i != last){
+                    maxi = max(maxi, points[0][i]);
+                }
+            }
+            return dp[day][last] = maxi;
+        }
+        int maxi = 0;
+        for(int i=0;i<3;i++){
+            if(i != last){
+                int activity = points[day][i] + f(day-1, i, points, dp);
+                maxi = max(maxi, activity);
+            }
+        }
+        return dp[day][last] = maxi;
+    }
+    int maximumPoints(vector<vector<int>>& points, int n) {
+        // Code here
+        vector<vector<int>>dp(n,vector<int>(4,-1));
+        return f(n-1,3,points,dp);
+    }
+};
+
+/*
+class Solution {
+  public:
+    int f(int day, int last, vector<vector<int>>& points, vector<vector<int>>& dp){
         
         if(dp[day][last] != -1) return dp[day][last];
         
@@ -23,7 +52,7 @@ class Solution {
         int maxi = 0;
         for(int i=0;i<3;i++){
             if(i != last){
-                int activity = points[day][i] + helper(day-1, i, points, dp);
+                int activity = points[day][i] + f(day-1, i, points, dp);
                 maxi = max(maxi, activity);
             }
         }
@@ -35,6 +64,7 @@ class Solution {
         return helper(n-1, 3, points, dp);
     }
 };
+*/
 
 //{ Driver Code Starts.
 int main() {
