a6637d196fd722104a7bec4bd8283b326f17cc66
Tushar Rathi <rathitushar021@gmail.com>
2023-06-22 22:57:28 +0545

    Time: 124 ms (71.41%), Space: 6.6 MB (93.58%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-    
-    // declare a dp
-    
-    int dp[1005][1005];
-    
-    int helper(string& str, int left, int right)
-    {
-        // base case
+    int longestCommonSubsequence(string s1, string s2) {
+        int x=s1.size();
+        int y=s2.size();
+        vector<int>prev(y+1,0);
+        vector<int>curr(y+1,0);
         
-        if(left > right)
-            return 0;
+        for(int j=0;j<=y;j++)prev[j]=0;
         
-        if(left == right)
-            return 1;
-        
-        // if already calculated
-        
-        if(dp[left][right] != -1)
-            return dp[left][right];
-        
-        // if characters are same
-        
-        if(str[left] == str[right])
-            return dp[left][right] = 2 + helper(str, left + 1, right - 1);
-        
-        else  // if characters are not same
-        {
-            return dp[left][right] = max(helper(str, left + 1, right), helper(str, left, right - 1));
+        for(int ind1=1;ind1<=x;ind1++){
+            
+            for(int ind2=1;ind2<=y;ind2++){
+          
+                if(s1[ind1-1]==s2[ind2-1]) curr[ind2]=1+prev[ind2-1];
+                else curr[ind2]=max(prev[ind2],curr[ind2-1]);
+            }
+            prev=curr;
         }
+        return prev[y];
     }
-    
-    int longestPalindromeSubseq(string str) {
-        
-        int n = str.size();
-        
-        // initialize dp
-        
-        memset(dp, -1, sizeof(dp));
-        
-        return helper(str, 0, n - 1);
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return longestCommonSubsequence(s,t);
     }
 };
