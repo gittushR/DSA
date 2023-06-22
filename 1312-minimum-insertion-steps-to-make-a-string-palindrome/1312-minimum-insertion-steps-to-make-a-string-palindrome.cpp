93043d4e9c2aace580e1b5adb8b7c3bbccab27fd
Tushar Rathi <rathitushar021@gmail.com>
2023-06-23 00:08:51 +0545

    Time: 76 ms (46.31%), Space: 6.7 MB (85.60%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,20 +1,35 @@
-class Solution {
-public:
-    int minInsertions(string s) {
-        int n = s.length();
-        vector<int> dp(n);
-        for (int i = n - 2; i >= 0; i--) {
-            int prev = 0;
-            for (int j = i + 1; j < n; j++) {
-                int temp = dp[j];
-                if (s[i] == s[j]) {
-                    dp[j] = prev;
-                } else {
-                    dp[j] = min(dp[j], dp[j-1]) + 1;
+class Solution
+{
+    public:
+        int longestCommonSubsequence(string s1, string s2)
+        {
+            int x = s1.size();
+            int y = s2.size();
+            vector<int> prev(y + 1, 0);
+            vector<int> curr(y + 1, 0);
+
+            for (int j = 0; j <= y; j++) prev[j] = 0;
+
+            for (int ind1 = 1; ind1 <= x; ind1++)
+            {
+
+                for (int ind2 = 1; ind2 <= y; ind2++)
+                {
+
+                    if (s1[ind1 - 1] == s2[ind2 - 1]) curr[ind2] = 1 + prev[ind2 - 1];
+                    else curr[ind2] = max(prev[ind2], curr[ind2 - 1]);
                 }
-                prev = temp;
+                prev = curr;
             }
+            return prev[y];
         }
-        return dp[n-1];
+    int longestPalindromeSubseq(string s)
+    {
+        string t = s;
+        reverse(t.begin(), t.end());
+        return longestCommonSubsequence(s, t);
+    }
+    int minInsertions(string s) {
+        return s.size()-longestPalindromeSubseq(s);
     }
 };
