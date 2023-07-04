a1ef648792503b4b68f7698177fe0bff739cb1a2
Tushar Rathi <rathitushar021@gmail.com>
2023-07-04 11:42:27 +0545

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum number of jumps/minimum-number-of-jumps.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+        int steps=0,jump=0,count=0;
+        for(int i=0;i<n-1;i++){
+            steps=max(steps,i+arr[i]);
+            if(i==jump){
+            count++;
+            jump=steps;
+            if(steps>=n-1)
+            return count;
+            
+            }
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
