6ffb762cb6ff9bc37894d0b77d55dd918f31b5da
Tushar Rathi <rathitushar021@gmail.com>
2023-07-04 12:15:39 +0545

    Time: 54 ms (45.83%), Space: 30.8 MB (97.91%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {      
+        if(start<0 || start >= arr.size() || arr[start]<0) return false;
+        if(arr[start]==0) return true;
+        arr[start] = -arr[start];
+        return  canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]);
+    }
+};
