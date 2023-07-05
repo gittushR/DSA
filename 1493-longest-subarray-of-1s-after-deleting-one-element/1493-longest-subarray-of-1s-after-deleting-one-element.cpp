f45913ec83c6f745230616be8281241dc03cb135
Tushar Rathi <rathitushar021@gmail.com>
2023-07-05 13:56:35 +0530

    Time: 57 ms (11.82%), Space: 36.5 MB (65.98%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+        int longestSubarray(vector<int> &nums)
+        {
+            if (nums.size() == 0) return 0;
+            vector<int> sums;
+            int sum = 0;
+            int arrsum=0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                arrsum+=nums[i];
+                if (nums[i] == 0)
+                {
+                    if (sum != 0) sums.push_back(sum);
+                    sums.push_back(0);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += nums[i];
+                }
+            }
+            if(arrsum==0)return 0;
+            sums.push_back(sum);
+           	// for(int i=0;i < sums.size();i++)cout<<sums[i]<<endl;
+            int ans = INT_MIN;
+            if (sums.size() == 1) return sums[0] - 1;
+            else if (sums.size() == 2)
+            {
+                sort(sums.begin(), sums.end());
+                return sums[1];
+            }
+            else
+            {
+                // int ind = 0;
+                // while (sums[ind] == 0)
+                // {
+                //     ind++;
+                // }
+                // cout<<"****"<<ind<<endl;
+                for (int i = 0; i < sums.size() - 2; i++)
+                {
+                    int s = sums[i] + sums[i + 2];
+                    ans = max(ans, s);
+                }
+            }
+            return ans;
+        }
+};
