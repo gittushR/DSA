b0182debc20d0e58d392a4fbe96f83bdc0020990
Tushar Rathi <rathitushar021@gmail.com>
2023-07-07 17:20:14 +0530

    Time: 126 ms (6.55%), Space: 56.2 MB (7.94%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int sum=0;
-        for(int i=0;i<weights.size();i++)sum+=weights[i];
-        
-        int s=0;
-        int e=sum;
-        int mid=(s+e)/2;
-        
-        int ans=-1;
-        
-        while(s<=e){
-            if (isPossible(weights,weights.size(),days,mid)){
-                ans=mid;
-                e=mid-1;
+    int fn(int cap, vector<int> arr){
+        int days=1;
+        int load=0;
+        for(int i=0;i<arr.size();i++){
+            if(load+arr[i]>cap){
+                days++;
+                load=arr[i];
             }else{
-                s=mid+1;
+                load+=arr[i];
             }
-            mid=(s+e)/2;
         }
-        return ans;
+        return days;
     }
-    bool isPossible(vector<int> arr, int n, int m,int mid){
-        int day=1;
-        int weightSum=0;
-        
-        for(int i=0;i<n;i++){
-            if(weightSum+arr[i]<=mid)weightSum+=arr[i];
-            else{
-                day++;
-                if(day>m || arr[i]>mid)return false;
-                weightSum=arr[i];
-            }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxi=-1e8,sum=0;
+        for(int i=0;i<weights.size();i++){
+            maxi=max(maxi,weights[i]);
+            sum+=weights[i];
         }
-        return true;
+        int low=maxi,high=sum;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int totalDays=fn(mid,weights);
+            if(totalDays<=days){
+                high=mid-1;
+            }else
+                low=mid+1;
+        }
+        return low;
     }
 };
