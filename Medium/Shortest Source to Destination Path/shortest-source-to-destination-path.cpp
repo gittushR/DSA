b2ef3d6841b94dd20c63e8321e9ed094b31c415a
Tushar Rathi <rathitushar021@gmail.com>
2023-08-02 16:28:52 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shortest Source to Destination Path/shortest-source-to-destination-path.cpp
+++ Medium/Shortest Source to Destination Path/shortest-source-to-destination-path.cpp
@@ -7,6 +7,8 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// User function Template for C++
+
 class Solution {
   public:
     int shortestDistance(int n, int m, vector<vector<int>> grid, int x, int y) {
