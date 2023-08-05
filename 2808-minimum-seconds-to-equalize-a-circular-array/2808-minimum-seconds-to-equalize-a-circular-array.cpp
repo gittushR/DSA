ba0aebe1e5708cb61be256953f1d2c03030472a9
Tushar Rathi <rathitushar021@gmail.com>
2023-08-05 22:51:53 +0530

    Time: 365 ms (100.00%), Space: 148.9 MB (14.29%) - LeetHub

--- 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
+++ 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     int minimumSeconds(vector<int>& nums) {
+//         int n=nums.size();map<int,int> mp;
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]]++;
+//         }
+//         cout<<mp.size()<<"****";
+//         if(mp.size()==1)return 0;
+//         if(mp.size()==n)return n/2;
+//         if(n==3)return 1;
+//         int maxNum=0,maxNumOcc=INT_MIN;
+//         for(auto it:mp){
+//             if(it.second>maxNumOcc){
+//                 maxNum=it.first;
+//                 maxNumOcc=it.second;
+//             }
+//         }
+//         cout<<maxNum<<"***";
+//         int prevIndex=-1,minDiff=1e9;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==maxNum){
+//                 if(prevIndex==-1){
+//                     prevIndex=i;
+//                 }else{
+//                     minDiff=min(minDiff,i-prevIndex);
+//                 }
+//             }
+//         }
+//         cout<<minDiff;
+//         if(minDiff==1)return n%2==0? (n-2)/2 : (n-1)/2;
+//         return (minDiff)/2;
         vector<int> v;
         for(auto i:nums){
             v.push_back(i);
