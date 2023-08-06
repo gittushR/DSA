f6040a7b8fa339fa48009377e2bfb82412c0d86e
Tushar Rathi <rathitushar021@gmail.com>
2023-08-06 17:31:02 +0530

    Time: 828 ms (46.77%), Space: 8 MB (70.80%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -5,18 +5,16 @@ public:
         if(index==word.size())return true;
         if(i < 0 or j < 0 or i >= n or j >= m) return false;
         
-        bool all = false;
-        if(word[index] == board[i][j])
-        {
+        bool check = false;
+        if(word[index] == board[i][j]){
             char temp = word[index];
             board[i][j] = '#';
-            all = dfs(i+1, j, index+1, board, word) or dfs(i-1, j, index+1, board, word) or 
+            check = dfs(i+1, j, index+1, board, word) or dfs(i-1, j, index+1, board, word) or 
                 dfs(i, j+1, index+1, board, word) or dfs(i, j-1, index+1, board, word);
-            
             board[i][j] = temp;
         }
         
-        return all;
+        return check;
     }
     bool exist(vector<vector<char>>& grid, string word) {
         int n = grid.size(), m = grid[0].size();
