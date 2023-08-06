4eca4707c2244568d1bcf50bca81c86fda0c0b3a
Tushar Rathi <rathitushar021@gmail.com>
2023-08-06 15:39:41 +0530

    Time: 3 ms (83.78%), Space: 11.7 MB (5.86%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -2,20 +2,16 @@
 #define mod 1000000007
 class Solution {
 public:
-    int n , l, k;
-    ll solve(int len , int num,vector<vector<int>> &dp){
+    ll solve(int len , int num,vector<vector<int>> &dp,int n, int l,int k){
         if(len==l)return num==n;
         if(dp[len][num]!=-1)return dp[len][num];
-        ll ans= (solve(len+1,num,dp)* max(0,num-k)) % mod;
-        ans+=(solve(len+1,num+1,dp)*(n-num))%mod;
+        ll ans= (solve(len+1,num,dp,n,l,k)* max(0,num-k)) % mod;
+        ans+=(solve(len+1,num+1,dp,n,l,k)*(n-num))%mod;
         ans=ans%mod;
         return dp[len][num]=ans;
     }
     int numMusicPlaylists(int n, int goal, int k) {
-        this-> n=n;
-        this->l=goal;
-        this->k=k;
         vector<vector<int>> dp(109,vector<int>(109,-1));
-        return solve(0,0,dp);
+        return solve(0,0,dp,n,goal,k);
     }
 };
