94b8e8cea236545b50b3de31a59a7b09cfad85b2
Tushar Rathi <rathitushar021@gmail.com>
2023-08-13 16:49:05 +0530

    Time: 138 ms (79.08%), Space: 92.3 MB (34.15%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -7,10 +7,10 @@ public:
         if(ind+1<n and nums[ind]==nums[ind+1]){
             if(helper(nums,ind+2,dp))return dp[ind]=true;
         }
-        if(ind+2<n and nums[ind]==nums[ind+2] and nums[ind]==nums[ind+1]){
+        if(ind+2<n and nums[ind]==nums[ind+1] and nums[ind]==nums[ind+2]){
             if(helper(nums,ind+3,dp))return dp[ind]=true;
         }
-        if(ind+2<n and nums[ind]+2==nums[ind+2] and nums[ind]+1==nums[ind+1]){
+        if(ind+2<n and nums[ind]+1==nums[ind+1] and nums[ind]+2==nums[ind+2]){
             if(helper(nums,ind+3,dp))return dp[ind]=true;
         }
         return dp[ind]=false;
@@ -21,26 +21,3 @@ public:
         return helper(nums,0,dp);
     }
 };
-// class Solution{
-// public:
-//     bool c(vector<int> &v, vector<int> &dp, int i){
-//         int n = v.size();
-//         if (i == v.size())return true;
-//         if (dp[i] != -1)return dp[i];
-//         if (i + 1 < n && v[i] == v[i + 1]){
-//             if (c(v, dp, i + 2))return dp[i] = true;
-//         }
-//         if (i + 2 < n && v[i] == v[i + 2] and v[i] == v[i + 1]){
-//             if (c(v, dp, i + 3))return dp[i] = true;
-//         }
-//         if (i + 2 < n && v[i] + 1 == v[i + 1] && v[i] + 2 == v[i + 2]){
-//             if (c(v, dp, i + 3))return dp[i] = true;
-//         }
-//         return dp[i] = false;
-//     }
-//     bool validPartition(vector<int> &v){
-//         int n = v.size();
-//         vector<int> dp(n + 1, -1);
-//         return c(v, dp, 0);
-//     }
-// };
