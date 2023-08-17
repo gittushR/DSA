c7202a3870d8a8b31c79e3766d01d2eca902c177
Tushar Rathi <rathitushar021@gmail.com>
2023-08-17 21:16:26 +0530

    Time: 217 ms (81.12%), Space: 134.7 MB (66.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,20 +1,24 @@
-class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-      deque<int>q;
-        int i=0,j=0;
-        vector<int>res;
-        while(j<arr.size()){
-            while(!q.empty() && q.back()<arr[j]) q.pop_back();
-            q.push_back(arr[j]);
-            if(j-i+1<k) j++;
-            else if(j-i+1==k){
-                res.push_back(q.front());
-                if(q.front()==arr[i]) q.pop_front();
-                i++;
-                j++;
+class Solution
+{
+    public:
+        vector<int> maxSlidingWindow(vector<int> &arr, int k)
+        {
+            deque<int> q;
+            int i = 0, j = 0;
+            vector<int> res;
+            while (j < arr.size())
+            {
+                while (!q.empty() && q.back() < arr[j]) q.pop_back();
+                q.push_back(arr[j]);
+                if (j - i + 1 < k) j++;
+                else if (j - i + 1 == k)
+                {
+                    res.push_back(q.front());
+                    if (q.front() == arr[i]) q.pop_front();
+                    i++;
+                    j++;
+                }
             }
+            return res;
         }
-        return res;
-    }
 };
