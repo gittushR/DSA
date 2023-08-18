b10fdefec20a9cd5b1d12d3fa0aa15747fd03378
Tushar Rathi <rathitushar021@gmail.com>
2023-08-18 15:00:56 +0530

    Time: 213 ms (77.92%), Space: 238.1 MB (5.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,20 +1,18 @@
 class Solution {
-public:
-    bool f(int ind, int target,vector<int>& nums, vector<vector<int>>&dp){
-        if(ind==0)return (target==nums[0]);
-        if(target==0)return true;
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        bool notTake=f(ind-1,target,nums,dp);
-        bool take=false;
-        if(target>=nums[ind])take=f(ind-1,target-nums[ind],nums,dp);
-        return dp[ind][target]=(take or notTake);
+    bool helper(int idx, vector<int> &nums, int take, int sum, vector<vector<int>> &dp){
+        if(idx==nums.size() or 2*take==sum) return 2*take==sum;
+        if(2*(take+nums[idx])<=sum){
+            if(dp[idx][take+nums[idx]]==-1) dp[idx][take+nums[idx]] = helper(idx+1, nums, take+nums[idx], sum, dp);
+            if(dp[idx][take+nums[idx]]) return true;
+        }
+        if(dp[idx][take]==-1) dp[idx][take] = helper(idx+1, nums, take, sum, dp);
+        return dp[idx][take];
     }
+public:
     bool canPartition(vector<int>& nums) {
-        int n=nums.size();
         int sum=0;
-        for(auto it:nums)sum+=it;
-        if(sum%2!=0)return false;
-        vector<vector<int>>dp(n,vector<int>((sum/2)+1,-1));
-        return f(n-1,sum/2,nums,dp);
+        for(auto &it : nums) sum+=it;
+        vector<vector<int>> dp(nums.size()+1, vector<int>(sum, -1));
+        return helper(0, nums, 0, sum, dp);
     }
 };
