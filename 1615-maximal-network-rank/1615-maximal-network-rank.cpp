e04ade3a1bd597a6517a1047e544f6a28ef49ae7
Tushar Rathi <rathitushar021@gmail.com>
2023-08-18 09:49:17 +0530

    Time: 102 ms (47.49%), Space: 39 MB (37.23%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> degree(n, 0);
-        set<pair<int, int>> road_set;
+        vector<int> indegree(n, 0);
+        set<pair<int, int>> roadset;
         
         for (auto& road : roads) {
-            degree[road[0]]++;
-            degree[road[1]]++;
-            road_set.insert({road[0], road[1]});
-            road_set.insert({road[1], road[0]});
+            indegree[road[0]]++;
+            indegree[road[1]]++;
+            roadset.insert({road[0], road[1]});
+            roadset.insert({road[1], road[0]});
         }
 
         int max_rank = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = i+1; j < n; ++j) {
-                int rank = degree[i] + degree[j];
-                if (road_set.find({i, j}) != road_set.end()) {
+                int rank = indegree[i] + indegree[j];
+                if (roadset.find({i, j}) != roadset.end()) {
                     rank--;
                 }
                 max_rank = max(max_rank, rank);
