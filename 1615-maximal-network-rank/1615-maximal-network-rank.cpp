bf2b4974a56a7a3afd492f56da93f599a6084900
Tushar Rathi <rathitushar021@gmail.com>
2023-08-18 14:33:18 +0530

    Time: 74 ms (79.85%), Space: 33.5 MB (54.83%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> indegree(n, 0);
-        set<pair<int, int>> roadset;
-        
-        for (auto& road : roads) {
-            indegree[road[0]]++;
-            indegree[road[1]]++;
-            roadset.insert({road[0], road[1]});
-            roadset.insert({road[1], road[0]});
+        set<pair<int, int>> st;
+        vector<int> inDeg(n, 0);
+        for(auto &it : roads){
+            st.insert({min(it[0], it[1]), max(it[0], it[1])});
+            inDeg[it[0]]++;
+            inDeg[it[1]]++;
         }
-
-        int max_rank = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i+1; j < n; ++j) {
-                int rank = indegree[i] + indegree[j];
-                if (roadset.find({i, j}) != roadset.end()) {
-                    rank--;
+        int ans=0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(st.find({i, j})!=st.end()){
+                    ans=max(ans, inDeg[i]+inDeg[j]-1);
                 }
-                max_rank = max(max_rank, rank);
+                else{
+                    ans=max(ans, inDeg[i]+inDeg[j]);
+                }
             }
         }
-
-        return max_rank;
+        return ans;
     }
 };
