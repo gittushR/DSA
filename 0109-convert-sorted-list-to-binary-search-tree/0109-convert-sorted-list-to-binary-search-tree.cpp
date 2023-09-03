bc651b40ada4b2e60b76a3934d04dfbbd4c31455
Tushar Rathi <rathitushar021@gmail.com>
2023-09-03 21:20:20 +0530

    Time: 28 ms (39.29%), Space: 28.3 MB (56.59%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,20 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* helper(vector<int> nums,int left,int right){
-        if(left>right) return NULL;
-        int mid=(left+right)/2;
-        TreeNode *root=new TreeNode(nums[mid]);
-        root->left=helper(nums,left,mid-1);
-        root->right=helper(nums,mid+1,right);
+    TreeNode* buildTree(ListNode*head, ListNode* tail){
+        if(head==tail)return NULL;
+        ListNode*slow=head;ListNode* fast=head;
+        while(fast!=tail and fast->next!=tail){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        TreeNode* root= new TreeNode(slow->val);
+        root->left=buildTree(head,slow);
+        root->right=buildTree(slow->next,tail);
         return root;
     }
     TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> nums;
-        while(head!=NULL){
-            nums.push_back(head->val);
-            head=head->next;
-        }
-        return helper(nums,0,nums.size()-1);
-        
+        ListNode* tail=NULL;
+        return buildTree(head,tail);
     }
 };
