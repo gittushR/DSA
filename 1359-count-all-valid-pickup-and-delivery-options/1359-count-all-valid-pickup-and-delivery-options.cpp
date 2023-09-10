acf04364b32a186242c4d9829af2b9d5aa61f7b5
Tushar Rathi <rathitushar021@gmail.com>
2023-09-10 12:29:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
     int countOrders(int n) {
-        long long dp[501]={0};
-        dp[1]=1*1ll;
-        dp[2]=6*1ll;
-        for(int i=3;i<=n;i++){
+        if(n==1)return 1;
+        long long prev,curr;
+        prev=1;
+        for(int i=2;i<=n;i++){
             int maxNum= 2*i-1;
             int permutations= maxNum * (maxNum+1)/2;
-            dp[i]=(dp[i-1]*permutations) % MOD;
+            curr=(prev*permutations) % MOD;
+            prev=curr;
         }
-        return dp[n];
+        return prev;
     }
 };
