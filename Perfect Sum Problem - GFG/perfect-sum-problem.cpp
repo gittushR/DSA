191f3499583e7ff5b5db2822a5386293a99c54ef
Tushar Rathi <rathitushar021@gmail.com>
2023-09-14 11:03:35 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -8,20 +8,16 @@ class Solution{
 	public:
 	int mod=1e9+7;
 	int findWaysUtil(int ind, int target, int arr[], vector<vector<int>> &dp){
-        if(ind == 0){
-            if(target==0 and arr[0]==0)return 2;
-            if(arr[0]==target or target==0)return 1;
+        if(ind==0){
+            if(arr[0]==0 and target==0)return 2;
+            if(target==arr[0] or target==0)return 1;
             return 0;
         }
-    
-        if(dp[ind][target]!=-1)
-            return dp[ind][target];
-        
-        int notTaken = findWaysUtil(ind-1,target,arr,dp);
-        int taken = 0;
-        if(arr[ind]<=target)
-            taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
-        return dp[ind][target]= (notTaken%mod + taken%mod)%mod;
+        if(dp[ind][target]!=-1)return dp[ind][target];
+        int notTaken=findWaysUtil(ind-1,target,arr,dp);
+        int taken=0;
+        if(target>=arr[ind])taken=findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        return dp[ind][target]=(taken%mod+notTaken%mod)%mod;
     }
 	int perfectSum(int arr[], int n, int k){
         vector<vector<int>> dp(n,vector<int>(k+1,-1));
