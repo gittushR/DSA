ee4a48a1552e71a54a6093f7b805f9273b96aa52
Tushar Rathi <rathitushar021@gmail.com>
2023-09-29 10:35:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number Of Enclaves/number-of-enclaves.cpp
+++ Medium/Number Of Enclaves/number-of-enclaves.cpp
@@ -11,55 +11,45 @@ using namespace std;
 class Solution {
   public:
     int numberOfEnclaves(vector<vector<int>> &grid) {
-        queue<pair<int,int>> q; 
-        int n = grid.size(); 
-        int m = grid[0].size(); 
-        int vis[n][m] = {0}; 
-        // traverse boundary elements
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                // first row, first col, last row, last col 
-                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
-                    // if it is a land then store it in queue
-                    if(grid[i][j] == 1) {
-                        q.push({i, j}); 
-                        vis[i][j] = 1; 
+        // Code here
+        int n=grid.size(),m=grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.push({i,j});
+                        vis[i][j]=1;
                     }
                 }
             }
         }
         
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, +1, +0, -1}; 
-        
-        while(!q.empty()) {
-            int row = q.front().first; 
-            int col = q.front().second; 
-            q.pop(); 
-            
-            // traverses all 4 directions
-            for(int i = 0;i<4;i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i]; 
-                // check for valid coordinates and for land cell
-                if(nrow >=0 && nrow <n && ncol >=0 && ncol < m 
-                && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = 1; 
+        vector<int> delRow={0,1,0,-1};
+        vector<int> delCol={1,0,-1,0};
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int row=it.first;
+            int col=it.second;
+            for(int i=0;i<4;i++){
+                int nrow=row+delRow[i];
+                int ncol=col+delCol[i];
+                if(nrow>=0 and ncol>=0 and nrow<n and ncol<m and vis[nrow][ncol]==0 and grid[nrow][ncol]==1){
+                    q.push({nrow,ncol});
+                    grid[nrow][ncol]=0;
+                    vis[nrow][ncol]=1;
                 }
             }
-            
         }
-        
-        int cnt = 0;
-        for(int i = 0;i<n;i++) {
-            for(int j = 0;j<m;j++) {
-                // check for unvisited land cell
-                if(grid[i][j] == 1 & vis[i][j] == 0) 
-                    cnt++; 
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 and vis[i][j]==0)cnt++;
             }
         }
-        return cnt; 
+        return cnt;
     }
 };
 
