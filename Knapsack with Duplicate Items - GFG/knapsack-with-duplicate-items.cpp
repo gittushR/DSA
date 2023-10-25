ba19676c164acfc170a393efaba2b065f7a2301b
Tushar Rathi <rathitushar021@gmail.com>
2023-10-25 23:52:48 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -7,26 +7,24 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution
-{
-    public:
-    //Function to return max value that can be put in knapsack of capacity W.
-    int f(int weight, int wt[], int val[],int index,vector<vector<int>>&dp){
-        if(index==0){
-            if(weight>=wt[0])return (weight/wt[0])*val[0];
+class Solution{
+public:
+    int helper(int weight, int val[], int wt[], int ind, vector<vector<int>> &dp){
+        if(ind==0){
+            if(weight>=wt[0]) return (weight/wt[0])*val[0];
             else return 0;
         }
-        if(dp[index][weight]!=-1)return dp[index][weight];
-        int notTake=f(weight,wt,val,index-1,dp);
-        int take=-1e9;
-        if(wt[index]<=weight)take=val[index]+f(weight-wt[index],wt,val,index,dp);
-        return dp[index][weight]=max(take,notTake);
+        if(dp[ind][weight]!=-1)return dp[ind][weight];
+        int notTaken = helper(weight,val,wt,ind-1,dp);
+        int taken = -1e9;
+        if(weight>=wt[ind])taken= val[ind]+helper(weight-wt[ind],val,wt,ind,dp);
+        return dp[ind][weight]=max(notTaken,taken);
     }
-    int knapSack(int n, int W, int val[], int wt[]) 
-    { 
-       // Your code here
-       vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return f(W,wt,val,n-1,dp);
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        vector<vector<int>> dp(N,vector<int>(W+1,-1));
+        return helper(W,val,wt,N-1,dp);
     }
 };
 
