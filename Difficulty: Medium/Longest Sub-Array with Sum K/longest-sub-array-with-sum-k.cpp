7971f4c49d58f6c11d36b22b2d30dee7ffd1e33c
Tushar Rathi <rathitushar021@gmail.com>
2024-08-02 14:31:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.cpp
@@ -9,22 +9,20 @@ class Solution{
     int lenOfLongSubarr(int arr[],  int n, int k) 
     { 
         // Complete the function
-        map<int,int> mpp;
         int maxLength=0;
         int sum=0;
+        unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
             sum+=arr[i];
             if(sum==k){
                 maxLength=max(maxLength,i+1);
             }
             int rem = sum-k;
-            if(mpp.find(rem)!=mpp.end()){
-                maxLength=max(maxLength,i-mpp[rem]);
+            if(mp.find(rem)!=mp.end()){
+                int l = i-mp[rem];
+                maxLength = max(maxLength,l);
             }
-            
-            if(mpp.find(sum)==mpp.end()){
-                mpp[sum]=i;
-            }
+            if(mp.find(sum)==mp.end())mp[sum]=i;
         }
         return maxLength;
     } 
