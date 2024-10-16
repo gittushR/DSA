6bf0b7cc287d68bcb1154c6676e6c8278ea6aa98
Tushar Rathi <rathitushar021@gmail.com>
2024-10-16 23:33:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Swaps/two-swaps.cpp
+++ Difficulty: Easy/Two Swaps/two-swaps.cpp
@@ -16,13 +16,15 @@ class Solution {
             swap(nums[i],nums[nums[i]-1]);
             count++;
         }
+        if(count==0)return true;
        bool flag=true;
        for(int i=0;i<n-1;i++){
            if(nums[i]>nums[i+1])flag=false;
        }
-       return (count<=2 && flag==true);
+       return (count==2 && flag==true);
     } 
 };
+
 
 //{ Driver Code Starts.
 
