f73ca2329ac82c69ca335ef49866f09c0b2c8548
Tushar Rathi <rathitushar021@gmail.com>
2024-10-17 22:56:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    
+    public:
+    
+    vector<Node*> alternatingSplitList(struct Node* head) {
+        
+        Node *list1Head = head;
+        Node *list2Head = head->next;
+        Node *list1 = head;
+        Node *list2 = head->next;
+        while(list1 && list2){
+            if(list1->next) list1->next = list1->next->next;
+            list1 = list1->next;
+            if(list2->next) list2->next = list2->next->next;
+            list2 = list2->next;
+        }
+        return {list1Head, list2Head};
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        struct Node* head = new Node(arr[0]);
+        struct Node* tail = head;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        vector<Node*> result = ob.alternatingSplitList(head);
+        printList(result[0]);
+        printList(result[1]);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
