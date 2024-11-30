517b06abf390bb3786762c037e0b0a95c882669a
Tushar Rathi <rathitushar021@gmail.com>
2024-11-30 17:06:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from a sorted doubly linked list/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -0,0 +1,159 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+void displayList(Node *head)
+{
+    //Head to Tail
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+
+class Solution
+{
+public:
+
+    Node * removeDuplicates(struct Node *head)
+    {
+        // Your code here
+        Node* temp1=head;
+        while(temp1!=nullptr and temp1->next!=nullptr){
+            Node*temp2=temp1->next;
+            while(temp2!=nullptr and temp2->data==temp1->data){
+                temp2=temp2->next;
+            }
+            temp1->next=temp2;
+            if(temp2) temp2->prev=temp1;
+            temp1=temp2;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    Solution obj;
+	    head=obj.removeDuplicates(head);
+	    
+	    
+	    if(verify(head))
+	    displayList(head);
+	    else
+	    cout<<"Your pointers are not correctly connected";
+ 
+	    cout<<endl;
+	
+cout << "~" << "\n";
+}
+	return 0;
+}
+// } Driver Code Ends
