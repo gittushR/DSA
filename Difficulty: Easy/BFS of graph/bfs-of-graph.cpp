cfa4d067b3850005ed48053df7f78d9afc2b44bd
Tushar Rathi <rathitushar021@gmail.com>
2025-02-21 17:40:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+        // Code here
+        vector<int>ans;
+        queue<int> q;
+        q.push(0);
+        unordered_map<int,bool> vis;
+        vis[0]=true;
+        while(q.size()){
+            auto it=q.front();
+            q.pop();
+            ans.push_back(it);
+            for(auto x: adj[it]){
+                if(!vis[x]){
+                    q.push(x);
+                    vis[x]=true;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        // Now using vector of vectors instead of array of vectors
+        vector<vector<int>> adj(V);
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
+        }
+
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
